3924e80607b297d071f8781f1caaa1ebfc94d4a5
Eva Reitzel <evareitzel@gmail.com>
2022-08-19 11:28:19 -0400

    lost on second .then using forEach()

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@
 // parse JSON resp
 // add img elements to DOM forEach img in array
 
+// on page load, fetch img using
+document.addEventListener('DOMContentLoaded', (e) => {
+  fetch('https://dog.ceo/api/breeds/image/random/4')
+  .then(resp => resp.json())
+  // .then(data => console.log(data))
+  .then(data) => { 
+    data.forEach(element => console.log(element))
+  // .then(data => {
+    // data.forEach(dog => console.log(dog))
+  }
+})
+  // const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
 
 //// Challenge 2
 //// Challenge 3
