ed71975fc0a7441ff363f4a5e12d9e7ab7e21864
Eva Reitzel <evareitzel@gmail.com>
2022-08-19 09:17:42 -0400

    Add pseudocode for Challenge 1/4

--- src/index.js
+++ src/index.js
@@ -1,66 +1,10 @@
-console.log('%c HI', 'color: firebrick')
-
-// Challenge 1
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-fetch(imgUrl)
-.then((resp) => resp.json())
-// .then(data => console.log(data))
-.then(data => data.message.forEach(image => addImage(image)));
-
-function addImage(image){
-  const container = document.querySelector('#dog-image-container');
-  const images = document.createElement('img');
-  images.src = image;
-  container.appendChild(images);
-}
-
-// Challenge 2
-const breedUrl = 'https://dog.ceo/api/breeds/list/all';
-fetch(breedUrl)
-.then((resp) => resp.json())
-.then(data => { 
-  const dogBreeds = data.message;
-  const objKeys = Object.keys(dogBreeds);
-
-  addElement();
+//// Challenge 1
+// on page load, fetch img using
+  // const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+// parse JSON resp
+// add img elements to DOM forEach img in array
 
 
-  // .then(data => data.message.key.forEach(breed => breeds(breed)));
-
-  function addElement(dogBreeds){
-    const container = document.querySelector('#dog-breeds');
-    const listItem = document.createElement('li');
-    // for (let i = 0; i<(dogBreeds.length); i++)
-    listItem.innerText = dogBreeds;
-    container.append(listItem)
-  }
-  objKeys.forEach(item => addElement(item))
-})
-
-// Challenge 3
-const item = document.querySelector('li');
-
-    // for (let i = 0; i<(dogBreeds.length); i++)
-    for (let i = 0; i < item.length; i++) {
-      item.addEventListener('click', function(e) {
-        e.target.style.color = 'red';
-      })
-    }
-
-// listItem.addEventListener('click', changeColor)
-
-function changeColor(evt) {
-  evt.target.style.color = 'red';
-  // const rndCol = red;
-  // item.style.color = rndCol;
-  
-  // const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-  // item.style.color = rndCol;
-  console.log(item);
-}
-
-// Challenge 4
-
-const dropdown = document.querySelector('#breed-dropdown');
-
-// dropdown.addEventListener(click)
+//// Challenge 2
+//// Challenge 3
+//// Challenge 4
